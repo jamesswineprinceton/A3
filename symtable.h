57fe5fb74f9cc453bdb5e1498d6213ae9b506c82
jamesswineprinceton <js2261@princeton.edu>
2025-03-19 15:49:00 -0400

    Hi

--- symtable.h
+++ symtable.h
@@ -8,18 +8,26 @@
 
 #include <stddef.h>
 
-struct SymTable;
+/* A SymTable_T is an unordered collection of key and value bindings. */
 typedef struct SymTable *SymTable_T;
+
+/*--------------------------------------------------------------------*/
      
 /* Returns a new SymTable object that contains no bindings, or NULL if 
 insufficient memory is available */
   SymTable_T SymTable_new(void);
 
+/*--------------------------------------------------------------------*/
+
 /* Frees all memory occupied by oSymTable */
   void SymTable_free(SymTable_T oSymTable);
 
+/*--------------------------------------------------------------------*/
+
 /* Returns the number of bindings in oSymTable */
   size_t SymTable_getLength(SymTable_T oSymTable);
+
+/*--------------------------------------------------------------------*/
 
 /* If oSymTable does not contain a binding with key pcKey, then 
 SymTable_put must add a new binding to oSymTable consisting of key pcKey 
@@ -30,24 +38,34 @@ available, then the function must leave oSymTable unchanged and return 0
   int SymTable_put(SymTable_T oSymTable,
      const char *pcKey, const void *pvValue);
 
+/*--------------------------------------------------------------------*/
+
 /* If oSymTable contains a binding with key pcKey, then SymTable_replace
 must replace the binding's value with pvValue and return the old value. 
 Otherwise it must leave oSymTable unchanged and return NULL.*/
   void *SymTable_replace(SymTable_T oSymTable,
      const char *pcKey, const void *pvValue);
 
+/*--------------------------------------------------------------------*/
+
 /* Returns 1 (TRUE) if oSymTable contains a binding whose key is pcKey, 
 and 0 (FALSE) otherwise */
   int SymTable_contains(SymTable_T oSymTable, const char *pcKey);
+
+/*--------------------------------------------------------------------*/
 
 /* Returns the value of the binding within oSymTable whose key is pcKey, 
 or NULL if no such binding exists */
   void *SymTable_get(SymTable_T oSymTable, const char *pcKey);
 
+/*--------------------------------------------------------------------*/
+
 /* If oSymTable contains a binding with key pcKey, then SymTable_remove 
 must remove that binding from oSymTable and return the binding's value. 
 Otherwise the function must not change oSymTable and return NULL. */
   void *SymTable_remove(SymTable_T oSymTable, const char *pcKey);
+
+/*--------------------------------------------------------------------*/
 
 /* Applies function *pfApply to each binding in oSymTable, passing 
 pvExtra as an extra parameter. That is, the function must call 
--- symtablelist.c
+++ symtablelist.c
@@ -3,21 +3,45 @@
 /* Author: James Swinehart                                            */
 /*--------------------------------------------------------------------*/
 
+#include <assert.h>
+#include <stdlib.h>
 #include "symtable.h"
 
+/*--------------------------------------------------------------------*/
+
+/* Each binding is stored in a node. Nodes are linked to form a list. */
+
 struct Node {
-    const char *key;
-    void *value;
-    struct Node *next;
+    /* The identifying key */
+    const char *pcKey;
+
+    /* The associated data */
+    const void *pvValue;
+
+    /* The address of the next node */
+    struct Node *psNext;
 };
 
-struct SymTable_T {
-    struct Node *first;
+/*--------------------------------------------------------------------*/
+
+/* A SymTable is a "dummy" node that points to the first node. */
+
+struct SymTable {
+    /* The address of the first node */
+    struct Node *psFirst;
 };
+
+/*--------------------------------------------------------------------*/
 
 SymTable_T SymTable_new(void) {
-    struct SymTable_T *s;
-    s = calloc(1, sizeof(*s)); 
-    if (s == NULL) return NULL;
-    return s;
+    SymTable_T oSymTable;
+
+    oSymTable = (SymTable_T)malloc(sizeof(struct SymTable));
+    if (oSymTable = NULL) return NULL;
+
+    oSymTable->psFirst = NULL;
+    return oSymTable;
 }
+
+/*--------------------------------------------------------------------*/
+
